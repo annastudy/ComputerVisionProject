51cc023d3640496ca21ed3a096d705fd322a49c5
annastudy <52180922+annastudy@users.noreply.github.com>
2022-02-17 17:50:31 -0700

    test

--- 02_image_classification_with_pytorch_and_ANN/04_pytorch_operations.py
+++ 02_image_classification_with_pytorch_and_ANN/04_pytorch_operations.py
@@ -14,34 +14,50 @@ x = torch.arange(6).reshape(3,2)
 print(x)
 
 print('Grabbing the right hand column values')
-
+print(x[:,1])
 
 print('Grabbing the right hand column as a (3,1) slice')
-
-
+print(x[:,1].reshape(3,1))
+print(x[:,1].view(3,1))
 # view() and reshape() do essentially the same thing by returning a reshaped tensor
 # without changing the original tensor in place.
 
 
 print('Views can infer the correct size')
-
-
+x =  torch.arange(9).reshape(3,3)
+print(x)
+print(x.shape)
+print(x.view(3,-1))#3 means the first dimension is 3; -1 means the 2nd dimension will be calculated.
 print('Tensor basic operations')
-
+a = torch.tensor([1,2,3], dtype=torch.float)
+b = torch.tensor([4,5,6], dtype=torch.float) #make sure the tensors are stored at the same place and are the same type
+print (a+b)
+print(a.add(b))
+print(a) #the value of a is not changed
 
 print('Changing a tensor in-place')
+a.add_(b)# the underscore "_" means change the value in-place
+print (a)#the value of a has been changed
 
 
 print('Dot products')
-
+a = torch.tensor([1,2,3], dtype=torch.float)
+b = torch.tensor([4,5,6], dtype=torch.float)
+print (a.dot(b))
 
 print('Matrix multiplication')
-
+a = torch.tensor([[0,2,4],[1,3,5]])
+b = torch.tensor([[6,7],[8,9],[10,11]])
+print(a.size())
+print(b.size())
+print(torch.mm(a,b))
+print(a.mm(b))
 
 print('L2 or Euclidean Norm')
 # The L2 norm calculates the distance of the vector coordinate from the origin of the vector space.
 # As such, it is also known as the Euclidean norm as it is calculated as the Euclidean distance from the origin.
 # The result is a positive distance value.
 
-
+x = torch.tensor([2.,5.,8.,14])
+print(x.norm())
 
--- 02_image_classification_with_pytorch_and_ANN/assignment2.py
+++ 02_image_classification_with_pytorch_and_ANN/assignment2.py
@@ -1,38 +1,66 @@
 # Import torch and NumPy
 
-
+import torch
+import numpy as np
 # Set the random seed for NumPy and PyTorch both to "42"
 #   This allows us to share the same "random" results.
-
+np.random.seed(42)
+torch.manual_seed(42)
 
 # Create a NumPy array called "arr" that contains 6 random integers between 0 (inclusive) and 5 (exclusive)
-
+arr = np.random.randint(0,5,6)
 
 # Create a tensor "x" from the array above
-
+x = torch.tensor(arr)
 
 # Change the dtype of x from 'int32' to 'int64'
-
+x = x.type(torch.LongTensor)
+print (x.dtype)
 
 # Reshape x into a 3x2 tensor
-
+x=x.reshape(3,2)
+print(x)
 
 # Return the left-hand column of tensor x
-
+print(x[:,0])
 
 # Without changing x, return a tensor of square values of x
-
+print(torch.square(x))
 
 # Create a tensor "y" with the same number of elements as x, that can be matrix-multiplied with x
-
-
+y = torch.ones(2,3,dtype=int)
+print (y)
 # Find the matrix product of x and y
-
+print (x.mm(y))
 
 # Create a Simple linear model using torch.nn
 # the model will take 1000 input and output 20 multi-class classification results.
 # the model will have 3 hidden layers which include 200, 120, 60 respectively.
+import torch.nn as nn
+import torch.nn.functional as F
+class MultiLayerPercepton(nn.Module):
+    def __init__(self, in_sz=1000, out_sz=20, layers=[200,120,60]):
+        super().__init__()
+
+        self.fc1 = nn.Linear(in_sz, layers[0])
+        self.fc2 = nn.Linear(layers[0],layers[1])
+        self.fc2 = nn.Linear(layers[1],layers[2])
+        self.fc4 = nn.Linear(layers[2], out_sz)
+
+    def forward(self, X):
+        X = F.relu(self.fc1(X))
+        X = F.relu(self.fc2(X))
+        X = F.relu(self.fc3(X))
+        X = F.log_softmax (self.fc4(X), dim=1)
+        # it is designed for multi-class classification and you need to specify the dimension that the calculation is on
+        return X
 
 
 
 # initiate the model and printout the number of parameters
+
+model = MultiLayerPercepton()
+print (model)
+
+for param in model.parameters():
+    print(param.numel())
